e18a34a9221134582c18a35674b825dff94ba549
arcadiak4 <celia.restes@protonmail.ch>
2024-04-29 23:20:29 +0200

    Menu responsive fixed but issues with z-index

--- script.js
+++ script.js
@@ -25,69 +25,36 @@ window.onload = () => {
       console.log('Arranging items with filter:', filterValue); // Débogage
       console.log(itemsGrid);
 
-      // const filterActiveItem = document.querySelector('.filter-item.active');
-      // filterActiveItem.classList.remove('active');
-      // console.log(filterActiveItem);
-      // this.classList.add('active');
-      // console.log(this);
-
       // Récupérer les éléments filtrés
       const filteredItems = itemsGrid.filteredItems.map(item => item.element);
       console.log('Éléments filtrés:', filteredItems);
     });
   });
+
+  // menu responsive
+  const menuBurger = document.querySelector(".default-icon");
+  const menuLinks = document.querySelector(".header-menu-section");
+
+  menuBurger.addEventListener('click', function() {
+    // open menu
+    if (this.src.includes('burger-bar.png')) {
+      menuLinks.classList.add('open-menu');
+      menuLinks.classList.remove('close-menu', false);
+        this.src = 'icons/close-window.png';
+    // close menu
+    } else {
+      this.src = 'icons/burger-bar.png';
+      menuLinks.classList.remove('open-menu');
+      menuLinks.classList.add('close-menu');
+    }
+  });
+
+  // apply style for close-menu then remove this class
+  menuLinks.addEventListener('transitionend', function(event) {
+    if (event.propertyName === 'opacity') {
+      menuLinks.classList.remove('close-menu');
+    }
+  });
+  
 }
-
-// window.onload = () => {
-//   const items = document.querySelector('.masonry-container');
-//   // let columnWidth = window.innerWidth >= 800 ? '.masonry-container-item' : 400; // Détermine la largeur initiale des colonnes en fonction de la taille de la fenêtre
-
-//   const masonry = new Masonry(items, {
-//     itemSelector: '.masonry-container-item',
-//     columnWidth: '.masonry-container-item',
-//     resize: true // active la redimension dynamique
-//   });
-
-//   masonry.on('layoutComplete', () => console.log('Layout Complete'));
-
-//   // // Écouteur d'événement pour ajuster la largeur des colonnes lors du redimensionnement de la fenêtre
-//   // window.addEventListener('resize', () => {
-//   //   // Met à jour la valeur de columnWidth en fonction de la largeur de la fenêtre
-//   //   columnWidth = window.innerWidth >= 800 ? '.masonry-container-item' : 400;
-//   //   masonry.layout(); // Réorganise la disposition Masonry avec la nouvelle largeur des colonnes
-//   // });
-// };
-
-// window.onload = () => {
-//   const items = document.querySelector('.masonry-container');
-
-//   const itemsGrid = new Isotope(items, {
-//     itemSelector: '.masonry-container-item',
-//     masonry: {
-//       columnWidth: '.masonry-container-item',
-//       resize: true // active la redimension dynamique
-//     }
-//     });
-
-//   document.addEventListener('click', (e) => {
-//     const targetElement = e.target;
-//     if (targetElement.closest('.filter-item')) {
-//       const filterItem = targetElement.closest('.filter-item');
-//       const filterValue = filterItem.filter;
-//       const filterActiveItem = document.querySelector('.filter-item.active');
-
-//       console.log(filterValue.);
-      
-//       if (filterValue !== undefined) {
-//         filterValue === "*" ? itemsGrid.arrange({filter: ``}) : itemsGrid.arrange({ filter: `[data-filter="${filterValue}"]` });
-//       }
-      
-//       filterActiveItem.classList.remove('active');
-//       filterItem.classList.add('active');
-
-//       e.preventDefault();
-//     }
-//   });
-// };
-
 
